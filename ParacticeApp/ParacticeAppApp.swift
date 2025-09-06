0e616779a467ae89060aa9dd8af407a6832f35bf
waseem <waseemabbaskpr786@gmail.com>
2025-09-06 03:15:56 -0700

    Initial Commit

---
+++ ParacticeApp/ParacticeAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  ParacticeAppApp.swift
+//  ParacticeApp
+//
+//  Created by Waseem Abbas on 06/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct ParacticeAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
